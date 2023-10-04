8ad44efe19ed1df4d6ec506485994ae459cfc4e3
NNYT1024 <gusrl1453@naver.com>
2023-10-04 13:16:22 +0900

    full전 업로드/ 변경ㄹ점 없음

--- MovieWepProject/src/main/webapp/WEB-INF/classes/KMDB/Data_KMDB.java
+++ MovieWepProject/src/main/webapp/WEB-INF/classes/KMDB/Data_KMDB.java
@@ -1,5 +1,30 @@
 package KMDB;
-//kmdb 반환값
-public class Data_KMDB {
 
+import org.json.*;
+
+
+
+//kmdb 반환값을 사용하기 쉽게 변환
+public class Data_KMDB {
+	public static void main(String[] args) {
+		String jsonString = "{\"name\": \"John\", \"age\": 30, \"city\": \"New York\"}";
+		Data_KMDB d = new Data_KMDB();
+		JSONObject test = d.Data_Processing(jsonString);
+	}
+	public JSONObject Data_Processing(String data) {
+		JSONObject resutl = new JSONObject();
+		JSONArray array = new JSONArray();
+		try {
+            JSONObject jsonObject = new JSONObject(data);
+
+            for (String key : jsonObject.keySet()) {
+                Object value = jsonObject.get(key);
+                System.out.println(key + " : " + value);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+		return resutl;
+	}
 }

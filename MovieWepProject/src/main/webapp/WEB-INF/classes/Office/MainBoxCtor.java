4f7370acfde02b18ea3de261842735e3f8ac6fa6
CSH <136411517+CSH-Ready@users.noreply.github.com>
2023-10-26 00:46:50 +0900

    그래프추가, 메인박스오피스 api호출,생성자,서블릿, jsp화면테스트추가

---
+++ MovieWepProject/src/main/webapp/WEB-INF/classes/Office/MainBoxCtor.java
@@ -0,0 +1,44 @@
+package Office;
+
+import java.util.List;
+
+public class MainBoxCtor {
+    private String rank;
+    private String movieNm;
+    private String openDt;
+    private String posterUrl;
+
+    public String getPosterUrl() {
+        return posterUrl;
+    }
+
+    public void setPosterUrl(String posterUrl) {
+        this.posterUrl = posterUrl;
+    }
+
+   
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public String getMovieNm() {
+        return movieNm;
+    }
+
+    public void setMovieNm(String movieNm) {
+        this.movieNm = movieNm;
+    }
+
+    public String getOpenDt() {
+        return openDt;
+    }
+
+    public void setOpenDt(String openDt) {
+        this.openDt = openDt;
+    }
+   
+}
